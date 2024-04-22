589d4aeda39eeeba5c3c6bb9beca8e08b3e02eee
poula9744 <poula9744@naver.com>
2024-04-22 11:32:19 +0900

    3

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -3,18 +3,11 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhonebookService;
 import com.javaex.vo.PhonebookVo;
-
-import ch.qos.logback.core.model.Model;
 
 @RestController
 public class PhonebookController {
@@ -23,7 +16,7 @@ public class PhonebookController {
 	private PhonebookService phonebookService;
 	
 	//리스트 
-	@GetMapping(value="api/phones")
+	@GetMapping(value="/api/phones")
 	public List<PhonebookVo> list(){
 		System.out.println("PhonebookController.list()");
 		List<PhonebookVo> phonebookList = phonebookService.exePhoneList();
